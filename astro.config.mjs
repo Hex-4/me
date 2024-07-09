047340130f840727abb0894c0dc2b6ba5bd7ec70
Hex-4 <121138331+Hex-4@users.noreply.github.com>
2024-07-08 21:19:17 -0600

    fix github

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://me.hex4.github.io',
+  site: 'https://hex4.github.io/me/',
 
 });
